9ace077bef89a270d3b5c3b63a331e0bcd180ff8
AlexxSome <alex.soto.29.9.88@gmail.com>
2023-11-20 01:28:55 -0300

    fix load data

--- src/main/java/cl/postulacion/nisum/LoadData.java
+++ src/main/java/cl/postulacion/nisum/LoadData.java
@@ -23,8 +23,18 @@ public class LoadData {
 
 
         User alex = new User("Alex Soto", "asoto@gmail.com", "A123qwe", phonesA);
-        User alex2 = new User("Alex 2", "aso2@gmail.com", "A123qwe", phonesA);
+        User alex2 = new User("Alex 2", "asoooo2@gmail.com", "A123qwe", phonesA);
         userService.createUser(alex);
+    }
 
+    @Bean
+    public void data2(){
+        Set<Phone> phonesA = new HashSet<>();
+        phonesA.add(new Phone("123123", "7", "52"));
+        phonesA.add(new Phone("123321", "6", "53"));
+
+
+        User alex2 = new User("Alex 2", "asoooo2@gmail.com", "A123qwe", phonesA);
+        userService.createUser(alex2);
     }
 }
